1ec63d159f91e0489f343cc56d15c6a00d2d329b
forosqueto32 <harrisonspeed1997@gmail.com>
2018-04-04 14:14:16 -0300

    Projeto Lesser v1.1

--- js/estilo.js
+++ js/estilo.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
     btnConfig();
     zonaPortofolioConf();
     scrollConfig();
@@ -7,28 +7,28 @@ window.onload = function() {
 }
 
 
-function scrollConfig(){
+function scrollConfig() {
     const target = document.querySelectorAll("[data-anime]");//pega todos elementos html que possuem este atributo e adiciona em um vetor
     const animationClass = "animate";
 
     function animeScroll() {
-        const windowTop = window.pageYOffset + (window.innerHeight*0.75); // Verificar a distancia do scroll da página de acordo com o topo
-        
+        const windowTop = window.pageYOffset + (window.innerHeight * 0.75); // Verificar a distancia do scroll da página de acordo com o topo
+
         target.forEach(function (element) {//parâmetro element == elemento do for each
             if (windowTop > element.offsetTop) {
                 element.classList.add(animationClass);
-            }else{
+            } else {
                 element.classList.remove(animationClass);
             }
         });
     }//fim animeScroll()
-   
-    if(target.length){//Animação não ocorrerá caso não haja elementos com "data-anime" na page
-        window.addEventListener("scroll", function() {
+
+    if (target.length) {//Animação não ocorrerá caso não haja elementos com "data-anime" na page
+        window.addEventListener("scroll", function () {
             animeScroll();
         });
     }
-    
+
 
 }
 
@@ -151,11 +151,11 @@ function btnConfig() {
     });
 }
 
-function aboutBtnConfig(){
-       const aux = document.querySelectorAll("span.seta-btn-zona-painel-right");
-       aux.forEach(function(element){
-            element.classList.add("seta-btn-zona-painel-down");
-       });
+function aboutBtnConfig() {//VERIFICAR
+    const aux = document.querySelectorAll("span.seta-btn-zona-painel-right");
+    aux.forEach(function (element) {
+        element.classList.add("seta-btn-zona-painel-down");
+    });
 }
 
 function zonaPortofolioConf() {
@@ -163,17 +163,17 @@ function zonaPortofolioConf() {
     var imgPortofolioDefaultPosition = [];
     var item;
     for (var i = 1; i <= 8; i++) {
-        item = {defaultLeft:"", defaultTop: ""};
+        item = { defaultLeft: "", defaultTop: "" };
         imgPortofolio.push(document.getElementById("portofolio-zona" + i));
-        item.defaultLeft = window.getComputedStyle(imgPortofolio[i-1]).left;
-        item.defaultTop = window.getComputedStyle(imgPortofolio[i-1]).top;
+        item.defaultLeft = window.getComputedStyle(imgPortofolio[i - 1]).left;
+        item.defaultTop = window.getComputedStyle(imgPortofolio[i - 1]).top;
         imgPortofolioDefaultPosition.push(item);
     }
 
 
     //ALLWORKS
     document.getElementById("portofolio-allworks").addEventListener("click", function () {
-        for(var i = 0; i < imgPortofolio.length; i++){
+        for (var i = 0; i < imgPortofolio.length; i++) {
             imgPortofolio[i].style.left = imgPortofolioDefaultPosition[i].defaultLeft;
             imgPortofolio[i].style.top = imgPortofolioDefaultPosition[i].defaultTop;
             imgPortofolio[i].style.opacity = "1";
@@ -186,27 +186,46 @@ function zonaPortofolioConf() {
     document.getElementById("portofolio-creative").addEventListener("click", function () {
 
         for (var i = 0; i < imgPortofolio.length; i++) {
-            if (i === 1 || i === 2 || i === 4 || i === 7) {//se o elemento se enquadrar na div que vou utilizar...
+            if (i == 1 || i == 2 || i == 4 || i == 7) {
                 imgPortofolio[i].style.opacity = "1";
                 imgPortofolio[i].style.transform = "scale(1,1)";
-            } else {
-                imgPortofolio[i].style.opacity = "0";
-                imgPortofolio[i].style.transform = "scale(0,0)";
+            }
+            switch (i) {
+                case 1:
+                    imgPortofolio[i].style.left = "0";
+                    imgPortofolio[i].style.top = "0%";
+                    break;
+
+                case 2:
+                    imgPortofolio[i].style.left = "25%";
+                    imgPortofolio[i].style.top = "0%";
+                    break;
+
+                case 4:
+                    imgPortofolio[i].style.left = "50%";
+                    imgPortofolio[i].style.top = "0%";
+                    break;
+
+                case 7:
+                    imgPortofolio[i].style.left = "75%";
+                    imgPortofolio[i].style.top = "0%";
+                    break;
+
+                default:
+                    imgPortofolio[i].style.opacity = "0";
+                    imgPortofolio[i].style.transform = "scale(0,0)";
+                    break;
             }
         }//fimfor
 
         //imgPortofolio[0]
-        imgPortofolio[1].style.left = "0";
-        imgPortofolio[1].style.top = "0%";
 
-        imgPortofolio[2].style.left = "25%";
-        imgPortofolio[2].style.top = "0%";
 
-        imgPortofolio[4].style.left = "50%";
-        imgPortofolio[4].style.top = "0%";
 
-        imgPortofolio[7].style.left = "75%";
-        imgPortofolio[7].style.top = "0%";
+
+
+
+
     });
     //PHOTOGRAPHY
     document.getElementById("portofolio-photography").addEventListener("click", function () {
@@ -217,21 +236,30 @@ function zonaPortofolioConf() {
             } else {
                 imgPortofolio[i].style.opacity = "1";
                 imgPortofolio[i].style.transform = "scale(1,1)";
+                switch (i) {
+                    case 4:
+                        imgPortofolio[4].style.top = "0";
+                        imgPortofolio[4].style.left = "0";
+                        break;
+                    case 5:
+                        imgPortofolio[5].style.top = "0";
+                        imgPortofolio[5].style.left = "25%";
+                        break;
+
+                    case 6:
+                        imgPortofolio[6].style.top = "0";
+                        imgPortofolio[6].style.left = "50%";
+                        break;
+                    case 7:
+                        imgPortofolio[7].style.top = "0";
+                        imgPortofolio[7].style.left = "75%";
+                        break;
+                }
             }
-        }//fimfor
 
-        imgPortofolio[4].style.top = "0";
-        imgPortofolio[4].style.left = "0";
-
-        imgPortofolio[5].style.top = "0";
-        imgPortofolio[5].style.left = "25%";
-
-        imgPortofolio[6].style.top = "0";
-        imgPortofolio[6].style.left = "50%";
-
-        imgPortofolio[7].style.top = "0";
-        imgPortofolio[7].style.left = "75%";
+        }
     });
+
     //WEB DEVELOPMENT
     document.getElementById("portofolio-webdevelopment").addEventListener("click", function () {
         for (var i = 0; i < imgPortofolio.length; i++) {
@@ -241,24 +269,34 @@ function zonaPortofolioConf() {
             } else {
                 imgPortofolio[i].style.opacity = "1";
                 imgPortofolio[i].style.transform = "scale(1,1)";
+                switch (i) {
+                    case 2:
+                        imgPortofolio[2].style.left = "0";
+                        imgPortofolio[2].style.top = "0";
+                        break;
+
+                    case 3:
+                        imgPortofolio[3].style.left = "25%";
+                        imgPortofolio[3].style.top = "0";
+                        break;
+
+                    case 5:
+                        imgPortofolio[5].style.left = "50%";
+                        imgPortofolio[5].style.top = "0";
+                        break;
+
+                    case 6:
+                        imgPortofolio[6].style.left = "75%";
+                        imgPortofolio[6].style.top = "0";
+                        break;
+
+                    case 7:
+                        imgPortofolio[7].style.left = "0";
+                        imgPortofolio[7].style.top = "50%";
+                        break;
+                }
             }
-        }//fimfor
-
-        imgPortofolio[2].style.left = "0";
-        imgPortofolio[2].style.top = "0";
-
-        imgPortofolio[3].style.left = "25%";
-        imgPortofolio[3].style.top = "0";
-
-        imgPortofolio[5].style.left = "50%";
-        imgPortofolio[5].style.top = "0";
-
-        imgPortofolio[6].style.left = "75%";
-        imgPortofolio[6].style.top = "0";
-
-        imgPortofolio[7].style.left = "0";
-        imgPortofolio[7].style.top = "50%";
-
+        }//fimfor     
     });
 
 }//fim zonaPortofolioConf
